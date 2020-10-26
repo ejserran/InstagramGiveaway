2747f8dc569945dcb4313cc81edc3ff3c8d92426
Jason Serrano <ejason.serrano@gmail.com>
2020-10-26 11:58:19 -0700

    Less calls to private api, better prints

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ import auth
 try:
     from instagram_private_api import (
         Client, __version__ as client_version)
+    from instagram_web_api import (
+        Client, __version__ as web_client_version)
 except ImportError:
     import sys
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
     from instagram_private_api import (
         Client, __version__ as client_version)
+    from instagram_web_api import (
+        Client, __version__ as web_client_version)
 
 
 def login(username, password):
@@ -18,20 +22,25 @@ def login(username, password):
     return insta_api
 
 
-def dump_json(filename, data):
-    with open(filename, 'w') as outfile:
+def dump_json(filename, data, permission='w'):
+    with open(filename, permission) as outfile:
         json.dump(data, outfile, indent=2)
+        outfile.write("\n")
 
 
 def handle_comments(vendor, entries, caption):
-    feed = private_api.username_feed(vendor)
+    print('\r' + vendor + " Comment Entries: ...", end='')
+    sys.stdout.flush()
+    feed = web_api.user_feed(get_user_id(vendor))
+    entry_count = 0
 
-    for post in feed["items"]:
-        if post["caption"] is None:
+    for post in feed:
+        if len(post["node"]["edge_media_to_caption"]["edges"]) <= 0:
             continue
 
-        if caption in post["caption"]["text"].lower():
-            comments = private_api.media_n_comments(post["id"], post["comment_count"])
+        if caption in post["node"]["edge_media_to_caption"]["edges"][0]["node"]["text"].lower():
+            comments = private_api.media_n_comments(post["node"]["id"],
+                                                    post["node"]["edge_media_preview_comment"]["count"])
             dump_json(vendor + '_comments.json', comments)
 
             for comment in comments:
@@ -41,32 +50,38 @@ def handle_comments(vendor, entries, caption):
                     if usr in entries:
                         if comment["text"] not in entries[usr]:
                             entries[usr].append(comment["text"])
+                            entry_count += 1
                     else:
-                        tmp = [comment["text"]]
-                        entries[usr] = tmp
+                        entries[usr] = [comment["text"]]
+                        entry_count += 1
+
+            print('\r' + vendor + " Comment Entries: " + str(entry_count))
+            sys.stdout.flush()
             break
 
 
 def get_user_id(user_name):
-    info = private_api.username_info(user_name)
-    return info["user"]["profile_pic_id"].split('_')[1]
+    info = web_api.user_info2(user_name)
+    return info["id"]
 
 
 def get_follower_count(user_name):
-    info = private_api.username_info(user_name)
-    return info["user"]["follower_count"]
+    info = web_api.user_info2(user_name)
+    return info["edge_followed_by"]["count"]
 
 
 def build_common_followers(vendors):
-    common_followers = {}
-    uuid = private_api.generate_uuid()
+    print("Gathering list of followers...")
+    common_followers = list()
 
     for vendor in vendors:
+        uuid = private_api.generate_uuid()
         count = get_follower_count(vendor)
-        print(vendor + " Count: " + str(count))
-        followers = {}
-        tmp_followers = {}
+        followers = list()
         max_id = 0
+        print('\r' + vendor + " Follower Count: 0/" + str(count), end='')
+        sys.stdout.flush()
+
         while count > len(followers):
             tmp = private_api.user_followers(get_user_id(vendor), uuid, max_id=max_id)
 
@@ -74,32 +89,26 @@ def build_common_followers(vendors):
                 followers.append(user["username"])
 
             max_id = tmp["next_max_id"]
-            print(len(followers))
+            if len(followers) >= count:
+                print('\r' + vendor + " Follower Count: " + str(count) + "/" + str(count))
+                sys.stdout.flush()
+            else:
+                print('\r' + vendor + " Follower Count: " + str(len(followers)) + "/" + str(count), end='')
+                sys.stdout.flush()
 
         if len(common_followers) is 0:
             common_followers = followers
             continue
 
-        if len(followers) < len(common_followers):
-            list1 = followers
-            list2 = common_followers
-        else:
-            list1 = common_followers
-            list2 = followers
-
-        for follower in list1:
-            if follower in list2:
-                tmp_followers.append(follower)
-
-        common_followers = tmp_followers
+        common_followers = list(set(followers) & set(common_followers))
     return common_followers
 
 
 def handle_followers(entries):
-    bad_list = {}
+    bad_list = set()
     for entry in entries:
         if entry not in common_followers_list:
-            bad_list.append(entry)
+            bad_list.add(entry)
     return bad_list
 
 
@@ -120,20 +129,22 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     private_api = login(args.username, args.password)
+    web_api = Client()
     vendors = ["thebabeswhostudy", "faithful.sweets", "femxquarters",
                "petitplatters", "designedby.lin", "designyourlifeplanner"]
     caption = "fall giveaway time!!"
     global common_followers_list
     common_followers_list = build_common_followers(vendors)
-    bad_entries = {}
-    dump_json('common_followers.json', common_followers_list)
+    bad_entries = set()
+    dump_json('common_followers.json', list(common_followers_list))
 
+    print("Gathering comment entries...")
     for vendor in vendors:
-        entries = {}
-        print(vendor)
+        entries = dict()
         handle_comments(vendor, entries, caption)
         finish_up(entries)
         dump_json(vendor + '_entries.json', entries)
-        bad_entries += handle_followers(entries)
+        bad_entries |= handle_followers(entries)
 
-    dump_json('bad_entries.json', bad_entries)
+    dump_json('bad_entries.json', list(bad_entries))
+    print("All done!")
